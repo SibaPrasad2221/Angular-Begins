c9dcff519b32cba2dfebf59ec5cd0f11cd05add9
sahoo <sahoosibaprasad2221@gmail.com>
2024-05-27 01:39:38 +0530

    added: header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent],
+  //as should do tree structure formation so under app root whichh is passed 
+  //in main.ts as main component where we have our sub component needs to be defined or nested comp need to be created
+  
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core"; //use this to make this header a actual component
+
+@Component({
+    selector: 'my-header', //using this selector name we can call this header component
+    standalone: true,//other components type are module based , module based or standalone comp
+    templateUrl: './header.component.html',
+    styleUrl: './app.component.css'
+})
+
+
+export class HeaderComponent{} //need to export this class to use this component
